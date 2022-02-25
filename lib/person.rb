2617a56950ff908168e86eafaf5b6ece739c635b
kshayes78 <89166628+kshayes78@users.noreply.github.com>
2022-02-24 20:53:06 -0500

    commit tree 41cadcc57b0461144cf5a60ce49cb8bdc9166930

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,41 @@
 # your code goes here
+class Person 
+    attr_reader :name, :happiness, :hygiene
+    attr_accessor :bank_account
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    
+    def happiness=(num)
+       @happiness = if num > 10
+        10
+       elsif num < 0
+        0
+       else
+        num
+    end
+   
+end
+
+# def hygiene=(num)
+#     @hygiene = if num > 10
+#                     10
+#                 elsif num < 0
+#                      0
+#                      else
+#                    num     
+#                  end
+
+
+# def clean?=(num)
+#     self.hygiene > 7
+# end
+
+# def happy?=(num)
+#    self.happiness > 7
+# end
